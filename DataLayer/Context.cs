b10569849a4bf744cbb5f6ed1af0e8d48a226164
Tristan Bettany <tristan.bettany@gmail.com>
2021-01-18 18:20:41 +0000

    Add Data Layer Project

---
+++ DataLayer/Context.cs
@@ -0,0 +1,12 @@
+﻿using DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
